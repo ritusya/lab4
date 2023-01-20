8f7714ed1c866eb912349544481f916fcc5c5eb4
ritusya <94692625+ritusya@users.noreply.github.com>
2023-01-20 13:48:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,63 +1,46 @@
-<?php 
-
-    require("db.php");
-
-    $users = $db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC);
-
+<?php
+  require_once 'connect.php';
+  $goods = mysqli_query($connect, "SELECT * FROM `goods`");
+  $goods = mysqli_fetch_all($goods);
+//  echo "<pre>";
+//  print_r($goods);
+//  echo "</pre>";
 ?>
 
 <!DOCTYPE html>
-<html lang="ru">
-
+<html lang="en">
 <head>
-
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel = "stylesheet" href = "src/css/style.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
-
-    <title>Лаба 5</title>
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <title>online shop</title>
 </head>
-
 <body>
-
-    <div>
-    <?php
-
-        foreach ($users as $user) 
-        {
-        ?>
-            
-            <div class = "island" id = "account">
-
-                <div class= "info">
-                    <span>user: </span> <span class="account_name"> <?php echo $user['name']?></span>
-                    <span>id: </span> <span class="account_id"> <?php echo $user['tag']?></span>
-                </div>
-
-                <div class = "buttons">
-                    <a id = "button" href="user.php?id=<?= $user['id']?>">switch</a>
-                    <a id = "button" href="update.php?id=<?= $user['id']?>">change</a>
-                    <a id = "button" href="delete.php?id=<?= $user['id']?>">delete</a>
-                </div>
-
-            </div>
-
+   <h1 class="title"> Online shop</h1>
+   <div class="list">
+       <a class="list" href="list.php">show list of all items</a>
+   </div>
+    <div class="goods">
         <?php
-        }
-
+        foreach ($goods as $item) {
         ?>
-       
+        
+        <div class="card">
+            <div class="item-image">
+                <img src="<?php echo $item[4] ?>" alt="">
+            </div>
+            <div class="item-info">
+                <h1><?php echo $item[1] ?></h1>
+                <h2>$<?php echo $item[3] ?></h2>
+            </div>
+            <h4><?php echo $item[2] ?></h4>
+            
+        </div>
+        <?php } ?>
+        
     </div>
-
-    <div id = "create">
-        <a id = "button" href="create.php">создать новый аккаунт</a>
-    </div>
-
+    
+    
 </body>
-
 </html>
