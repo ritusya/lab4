0c875741f9f52d1d7cad02b114f2416e609645ac
ritusya <94692625+ritusya@users.noreply.github.com>
2023-01-20 13:52:58 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,63 +1,35 @@
 <?php
+  require_once 'connect.php';
+  $goods_id = $_GET['id'];
+  $good = mysqli_query($connect, "SELECT * FROM `goods` WHERE `id` = '$goods_id' ");
+  $good = mysqli_fetch_assoc($good);
+  // print_r($good);
 
-    require("db.php");
-
-    if ($_SERVER['REQUEST_METHOD'] === 'GET')
-    {
-        $id = $_GET['id'];
-        $users = $db->query("SELECT * FROM users WHERE id=$id")->fetchAll(PDO::FETCH_ASSOC);
-        $users = $users[0];
-    }
-
-    else if ($_SERVER['REQUEST_METHOD'] === 'POST')
-    {
-        $id = $_POST['id'];
-        $username = $_POST['username'];
-        $usertag = $_POST['usertag'];
-
-        $db->query("UPDATE users SET name = '$username', tag = '$usertag' WHERE id=$id");
-
-        header('location:index.php');
-    }
 ?>
 
+
 <!DOCTYPE html>
-<html lang="ru">
-
+<html lang="en">
 <head>
-
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel = "stylesheet" href = "src/css/style.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet">
-
-    <title>Лаба 5</title>
-
+    <link rel="stylesheet" href="style.css">
+    <title>Update</title>
 </head>
-
 <body>
-    
-    <header>
-
-        <a id = "back" href = "index.php"> <span>назад</span> </a>
-
-    </header>
-
-    <div class = "island" id = "create_island">
-
-        <form action="update.php" method="POST">
-
-        <div> <input type = "text" name = "username" required value="<?php echo $users['name'] ?>"/> </div>
-            <div> <input type = "text" name = "usertag" required value="<?php echo $users['tag'] ?>"/> </div>
-            <div> <input type="hidden" name="id" value="<?php echo $users['id'] ?>" /> </div>
-            <div> <input id = "create_button" type = "submit" name="submit" value = "change" /> </div>
-        </form>
-
-    </div>
-
+    <div class="upd-box">
+    <form action="update-method.php" method="post">
+        <input type="hidden" name="id" value="<?= $good['id'] ?>">
+        <p>Name</p>
+        <input type="text" name="title" value="<?= $good['name'] ?>">
+        <p>Description</p>
+        <textarea name="description"><?= $good['description'] ?></textarea>
+        <p>Price</p>
+        <input type="number" name="price" value="<?= $good['price'] ?>">
+        <p>Image</p>
+        <input type="text" name="image" value="<?= $good['image'] ?>">
+        <button type="submit">Update</button>
+    </form>
+    <img src="<?php echo $good['image'] ?>" alt="">
+</div>
 </body>
-
 </html>
