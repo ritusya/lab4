b3c2db61496132629c480b98a5655f8ab100d978
ritusya <94692625+ritusya@users.noreply.github.com>
2023-01-20 14:28:39 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-  $connect = mysqli_connect('localhost', 'root', 'root', 'lab5');
+  $connect = mysqli_connect('localhost', 'root', 'root', 'lab4');
   if(!$connect) {
     die('error N0_1 with connection');
   }
